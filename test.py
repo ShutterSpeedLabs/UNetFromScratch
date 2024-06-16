4b5f225457923b2f8f6a169da2bb0fb56b8727a4
ShutterSpeed Labs <shutterspeedlabs@gmail.com>
2024-06-16 12:09:30 +0530

    model

--- test.py
+++ test.py
@@ -16,24 +16,30 @@ class DoubleConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
-    
+
 class UNET(nn.Module):
-    def __init__(self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],):
+    def __init__(
+            self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],
+    ):
         super(UNET, self).__init__()
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        #Down part of UNET
+        # Down part of UNET
         for feature in features:
             self.downs.append(DoubleConv(in_channels, feature))
             in_channels = feature
 
-        #up part of UNET
+        # Up part of UNET
         for feature in reversed(features):
-            self.ups.append(nn.ConvTranspose2d(feature*2, feature, kernel_size=2, stride=2))
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    feature*2, feature, kernel_size=2, stride=2,
+                )
+            )
             self.ups.append(DoubleConv(feature*2, feature))
-        
+
         self.bottleneck = DoubleConv(features[-1], features[-1]*2)
         self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
@@ -54,22 +60,17 @@ class UNET(nn.Module):
 
             if x.shape != skip_connection.shape:
                 x = TF.resize(x, size=skip_connection.shape[2:])
-            
+
             concat_skip = torch.cat((skip_connection, x), dim=1)
             x = self.ups[idx+1](concat_skip)
 
         return self.final_conv(x)
-        
+
 def test():
     x = torch.randn((3, 1, 161, 161))
     model = UNET(in_channels=1, out_channels=1)
     preds = model(x)
-    print(preds.shape)
-    print(x.shape)
-    assert preds.shape == x.shape 
+    assert preds.shape == x.shape
 
-
-if __name__=="__main__":
-    test()      
-
-
+if __name__ == "__main__":
+    test()
